56b74246bcc37682ee15d7f7d00c9dd6d4071d9b
Emmanuel Oben <obentech@gmail.com>
2022-01-28 03:05:42 +0100

    Complete Simple Interest Calculator

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+window.onload = function(){
+    document.getElementById("rate").onchange = function(e){
+       updateRate();
+    };
+    updateRate();
+};
+
+function updateRate(){
+    var target = document.getElementById("rate");
+    var counter = document.getElementById("rate_val");
+    counter.innerHTML = target.value + "%";
+}
+
+function compute(){
+    event.preventDefault();
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear() + parseInt(years);
+
+    if (!principal || principal == 0){
+        alert("Enter a positive number");
+        document.getElementById("result").innerHTML = "";
+        document.getElementById("principal").focus();
+        return;
+    }
+
+    document.getElementById("result").innerHTML =  "If you deposit <mark>" + principal + "</mark>,<br />at an interest rate of <mark>" + rate + 
+        "%</mark><br />You will receive an amount of <mark>" + interest + "</mark>,<br /> in the year <mark>" + year + "</mark>";
+
 }
         
